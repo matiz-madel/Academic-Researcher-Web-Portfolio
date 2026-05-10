5f0b5ecc0709c62c81367266494619c784b19489
matiz-madel <matiz.madel@ufpr.br>
2026-05-09 23:26:49 -0300

    installs Sentry

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\SetLocale;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +16,5 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append:SetLocale::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        Integration::handles($exceptions);
     })->create();

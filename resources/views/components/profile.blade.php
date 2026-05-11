d20bce5ec27ad0de3d8454d9f937a83a69fdb82c
matiz-madel <matiz.madel@ufpr.br>
2026-05-11 17:10:10 -0300

    Enhance external link appearance

--- resources/views/components/profile.blade.php
+++ resources/views/components/profile.blade.php
@@ -138,8 +138,8 @@
                         <a href="{{ $link->url }}"
                             target="_blank"
                             rel="me noopener noreferrer"
-                            style="background: linear-gradient(180deg, transparent 95%, color-mix(in srgb, {{ $link->color }}, currentColor 25%) 95%);"
-                            class="inline-flex items-center px-4 py-2 leading-none rounded-full border-2 border-slate-200 dark:border-slate-700 text-slate-900 dark:text-white text-sm font-bold hover:scale-105 transition-all duration-200">
+                            style="border-color: color-mix(in srgb, {{ $link->color }}, currentColor 25%);"
+                            class="inline-flex items-center px-4 py-2 leading-none rounded-full border-2 bg-transparent text-slate-900 dark:text-white text-sm font-bold hover:scale-105 transition-all duration-200">
                             {{ $link->label }}
                         </a>
                     @endforeach
